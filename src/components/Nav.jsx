dbb4248d152f48c985c8652cbfecfe3608d11ae0
Max <myshailik@gmail.com>
2022-09-03 18:53:16 +0300

    add example in Nav with situation if you have 2+ classes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,14 +4,20 @@ const Nav = () => {
   return (
     <nav className={s.nav}>
       <div className={s.col}>
-        <div className={s.item}>
-          <a href="#s">Profile</a>
+        <div className={`${s.item}`}>
+          <a className={s.hover} href="#s">
+            Profile
+          </a>
         </div>
         <div className={s.item}>
-          <a href="#s">Contacts</a>
+          <a className={s.hover} href="#s">
+            Contacts
+          </a>
         </div>
         <div className={s.item}>
-          <a href="#s">Messages</a>
+          <a className={s.hover} href="#s">
+            Messages
+          </a>
         </div>
       </div>
     </nav>
