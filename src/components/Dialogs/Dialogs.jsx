48be1243ab927f97e49f133ad1389666a54fff91
Max <myshailik@gmail.com>
2022-09-10 12:16:31 +0300

    mini refactoring css

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,5 +1,16 @@
+import s from "./Dialogs.module.css";
+
 const Dialogs = () => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogs__item}>
+        <div className={s.dialog}>Max</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Sdjskkd asdlld sldldlld</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,14 @@ import s from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={s.header}>
       <div className="container">
         <div className={s.content}>
           <img
             className={s.logo}
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Logo_NIKE.svg/640px-Logo_NIKE.svg.png"
-          ></img>
+            alt="#s"
+          />
         </div>
       </div>
     </header>
